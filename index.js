8328998fe6932899bea7ba66446ab70e720e8174
Mayank Arora <mayankarora2302@gmail.com>
2025-05-21 15:08:45 +0530

    dice-game

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ document.querySelector(".img2").setAttribute("src",images[randomNumber2]);
 if (randomNumber1>randomNumber2){
     document.querySelector(".container h1").innerHTML="Player 1 Wins!!!";
 }else if (randomNumber2>randomNumber1){
-    document.querySelector(".container h1").innerHTML="Player 1 Wins!!!";
+    document.querySelector(".container h1").innerHTML="Player 2 Wins!!!";
 }else {
     document.querySelector(".container h1").innerHTML="Draw!!!";
 }
